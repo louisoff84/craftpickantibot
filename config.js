2c95f295558157e508844e7bb163ac46694da6b2
louisoff84 <195103747+louisoff84@users.noreply.github.com>
2026-07-28 14:49:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Après avoir hébergé le dossier backend, indique son URL publique HTTPS ici.
 // Exemple : window.CRAFTPICK_API_URL = "https://captcha-api.craftpick.fr";
-window.CRAFTPICK_API_URL = "";
+window.CRAFTPICK_API_URL = "http://82.26.80.20:3000";
